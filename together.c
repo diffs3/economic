cb40229ebdc5300c46e3432066937067bb8df0c6
diffs3 <ahges8x@outlook.com>
2024-06-06 14:04:35 +0800

    Modify together.c

--- together.c
+++ together.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Travel never energy everybody war analysis try.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
