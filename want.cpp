f31609bf604f801f6fb0702c7e4df2cc6bb9e2f8
diffs3 <ahges8x@outlook.com>
2024-07-12 11:54:45 +0800

    Modify want.cpp

--- want.cpp
+++ want.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Example out better.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
