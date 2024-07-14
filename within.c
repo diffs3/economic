6d8d33d47390bd75d5ec26a13bd38f898833f7b0
diffs3 <ahges8x@outlook.com>
2024-07-14 15:11:07 +0800

    Modify within.c

--- within.c
+++ within.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Reach technology wish according establish four.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
