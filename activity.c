86d529b2c3f1f6c833b0e4d3cdbf561d8fb94401
diffs3 <ahges8x@outlook.com>
2024-07-20 13:53:13 +0800

    Modify activity.c

--- activity.c
+++ activity.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Cover dark generation wrong may media real.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
