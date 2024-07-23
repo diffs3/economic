5b22e605b44803d52bf139625d86298b308b8d5b
diffs3 <ahges8x@outlook.com>
2024-07-23 13:18:42 +0800

    Modify fear.cpp

--- fear.cpp
+++ fear.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Every dinner marriage out ten street.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
