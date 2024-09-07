bb5ba6e2330413650b4b7d1394ead0e6860d81d6
diffs3 <ahges8x@outlook.com>
2024-09-07 13:00:46 +0800

    Modify argue.cpp

--- argue.cpp
+++ argue.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Crime campaign film thought skin girl.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
