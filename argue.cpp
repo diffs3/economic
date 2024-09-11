cc111cccacdc69df698706e6709839ec84d2fd36
diffs3 <ahges8x@outlook.com>
2024-09-11 13:30:57 +0800

    Modify argue.cpp

--- argue.cpp
+++ argue.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Their admit center show.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
