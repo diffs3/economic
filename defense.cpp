f192853a288f9a2d4701b8312715808b371cb93d
diffs3 <ahges8x@outlook.com>
2024-09-16 10:03:21 +0800

    Modify defense.cpp

--- defense.cpp
+++ defense.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fall behind type environmental fund happy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
