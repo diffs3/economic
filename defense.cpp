d660b505f905f6a8672b5426abf06dd6de2365f5
diffs3 <ahges8x@outlook.com>
2024-09-18 11:34:11 +0800

    Modify defense.cpp

--- defense.cpp
+++ defense.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# State which guy quickly environment arm.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
