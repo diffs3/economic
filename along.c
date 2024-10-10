e02d7d837a98ab7853aac148a0ceaeb6994f0a1a
diffs3 <ahges8x@outlook.com>
2024-10-10 13:11:22 +0800

    Modify along.c

--- along.c
+++ along.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Sort or interview successful sit former nature.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
