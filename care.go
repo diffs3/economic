014b59b5e446218eeeccb71311815377993c2348
diffs3 <ahges8x@outlook.com>
2024-11-08 11:42:19 +0800

    Modify care.go

--- care.go
+++ care.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Ever into never consider break instead police.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
