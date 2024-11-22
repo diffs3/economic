87d491517e8bb8a01b1bec81f1b0e0c194737a2d
diffs3 <ahges8x@outlook.com>
2024-11-22 13:38:49 +0800

    Modify off.cpp

--- off.cpp
+++ off.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fly man second.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
