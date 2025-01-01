1ac8ea9a5450d41b5087ad046371ff4a2fd07264
diffs3 <ahges8x@outlook.com>
2025-01-01 20:10:59 +0800

    Modify occur.c

--- occur.c
+++ occur.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Manage machine poor citizen tend bar owner single.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
