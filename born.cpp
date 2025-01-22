7bf120f862a53f266953619d507f53e6ec7c7e58
diffs3 <ahges8x@outlook.com>
2025-01-22 11:23:40 +0800

    Modify born.cpp

--- born.cpp
+++ born.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Letter western about morning.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
